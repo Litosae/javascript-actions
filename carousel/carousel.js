f76b056bf62c3b02638cece23c67dfbc68b6fb18
Lito Karageorgou <13-13995@saeinstitute.edu>
2025-02-14 14:26:36 +0200

    Add start and stop buttons to the carousel

--- carousel/carousel.js
+++ carousel/carousel.js
@@ -13,47 +13,65 @@ function showSlides(startIndex) {
         if (i >= startIndex && i < startIndex + visibleSlides){
             slides[i].classList.add('displayBlock');
             slides[i].classList.remove('displayNone');
-        } else{
+        } else if (startIndex + visibleSlides > slides.length && i < (startIndex + visibleSlides) % slides.length) {
+            slides[i].classList.add('displayBlock');
+            slides[i].classList.remove('displayNone');
+        } else {
             slides[i].classList.add('displayNone');
             slides[i].classList.remove('displayBlock');
         }
     }
 }
 
-
 // Function that loops through the slides
 function startLoop(){
     currentIndex++;
-    if (currentIndex > slides.length - visibleSlides){
+    if (currentIndex >= slides.length){
         currentIndex = 0;
     }
     showSlides(currentIndex);
     console.log(currentIndex);
 }
 
-
 // Set the timer to 7 seconds
 let timer = setInterval(startLoop, 7000);
 
-
 // Previous/Next buttons
-
 function previousSlide(){
     clearInterval(timer);
     currentIndex--;
     if (currentIndex < 0){
-        currentIndex = slides.length - visibleSlides;
+        currentIndex = slides.length - 1;
     }
     showSlides(currentIndex);
     console.log(currentIndex);
+    timer = setInterval(startLoop, 7000); // Restart the timer
 }
 
 function nextSlide(){
     clearInterval(timer);
     currentIndex++;
-    if (currentIndex > slides.length - visibleSlides){
+    if (currentIndex >= slides.length){
         currentIndex = 0;
     }
     showSlides(currentIndex);
     console.log(currentIndex);
+    timer = setInterval(startLoop, 7000); // Restart the timer
 }
+
+// Start/Stop buttons
+
+let activeInterval;
+
+function startCarousel(){
+    if(!activeInterval){
+        activeInterval = setInterval(startLoop, 7000);
+    }
+}
+
+function stopCarousel(){
+    clearInterval(activeInterval);
+    activeInterval = null;
+}
+
+// Thumbnails
