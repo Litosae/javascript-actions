3aca78c6631bda5d251f8e7ad69dee701693238f
Lito Karageorgou <13-13995@saeinstitute.edu>
2025-02-15 03:59:38 +0200

    Fix button and timer range bugs

--- carousel/carousel.js
+++ carousel/carousel.js
@@ -1,16 +1,11 @@
-// Get the images from the DOM
 const slides = document.querySelectorAll('.slide');
-
-// Set the current slide to 0
 let currentIndex = 0;
-
-// Number of slides to show
 const visibleSlides = 3;
+let activeInterval;
 
-// Function that shows the active slide and hides the rest
 function showSlides(startIndex) {
-    for (let i = 0; i < slides.length; i++){
-        if (i >= startIndex && i < startIndex + visibleSlides){
+    for (let i = 0; i < slides.length; i++) {
+        if (i >= startIndex && i < startIndex + visibleSlides) {
             slides[i].classList.add('displayBlock');
             slides[i].classList.remove('displayNone');
         } else if (startIndex + visibleSlides > slides.length && i < (startIndex + visibleSlides) % slides.length) {
@@ -23,61 +18,66 @@ function showSlides(startIndex) {
     }
 }
 
-// Function that loops through the slides
-function startLoop(){
+function startLoop() {
     currentIndex++;
-    if (currentIndex >= slides.length){
+    if (currentIndex >= slides.length) {
         currentIndex = 0;
     }
     showSlides(currentIndex);
     console.log(currentIndex);
 }
 
-// Set the timer to 7 seconds
 let timer = setInterval(startLoop, 7000);
 
 // Previous/Next buttons
-function previousSlide(){
+
+function previousSlide() {
     clearInterval(timer);
     currentIndex--;
-    if (currentIndex < 0){
+    if (currentIndex < 0) {
         currentIndex = slides.length - 1;
     }
     showSlides(currentIndex);
     console.log(currentIndex);
-    timer = setInterval(startLoop, 7000); // Restart the timer
 }
 
-function nextSlide(){
+function nextSlide() {
     clearInterval(timer);
     currentIndex++;
-    if (currentIndex >= slides.length){
+    if (currentIndex >= slides.length) {
         currentIndex = 0;
     }
     showSlides(currentIndex);
     console.log(currentIndex);
-    timer = setInterval(startLoop, 7000); // Restart the timer
 }
 
 // Start/Stop buttons
 
-let activeInterval;
-
-function startCarousel(){
-    if(!activeInterval){
-        activeInterval = setInterval(startLoop, 7000);
+function startCarousel(duration = 7000) {
+    if (!activeInterval) {
+        activeInterval = setInterval(startLoop, duration);
     }
 }
 
-function stopCarousel(){
+function stopCarousel() {
     clearInterval(activeInterval);
     activeInterval = null;
 }
 
 // Thumbnails
 
-function showThumbnail(index){
+function showThumbnail(index) {
     clearInterval(timer);
     currentIndex = index;
     showSlides(currentIndex);
 }
+
+// Timer
+
+const timerInput = document.getElementById('timer');
+
+timerInput.addEventListener('change', () => {
+    const newTimer = timerInput.value * 1000;
+    stopCarousel();
+    startCarousel(newTimer);
+});
